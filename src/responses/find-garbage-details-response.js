f5796fc4fda0e3f609ceb301dcdd5ff23395f088
Sergey Mirzoyan <sergey.mirzoyan1991@gmail.com>
2021-07-17 19:30:40 +0400

    change the adapters

--- src/responses/find-garbage-details-response.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var FindGarbageDetailsResponse = /** @class */ (function () {
-    function FindGarbageDetailsResponse(items) {
-        this.items = items;
-    }
-    return FindGarbageDetailsResponse;
-}());
-exports["default"] = FindGarbageDetailsResponse;
--- src/responses/find-garbage-response.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var FindGarbageResponse = /** @class */ (function () {
-    function FindGarbageResponse(count) {
-        this.count = count;
-    }
-    return FindGarbageResponse;
-}());
-exports["default"] = FindGarbageResponse;
